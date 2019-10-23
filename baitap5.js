30d3f031d78a57c5bc302080ba5628db0d05cb7e
hienjerry0301 <54108604+hienjerry0301@users.noreply.github.com>
2019-10-23 10:18:40 +0700

    Add files via upload

---
+++ baitap5.js
@@ -0,0 +1,7 @@
+var loibaihat = ["kia","con","buom","vang","kia","con","buom","xanh"]
+
+for ( let i = 0; i < loibaihat.length ; i++){
+    setTimeout(function(){
+        console.log(loibaihat[i], )
+    }, 1000)
+}
