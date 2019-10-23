52b04bb9e5be4eb229ca106acc1bbfc5df1c0b19
hienjerry0301 <54108604+hienjerry0301@users.noreply.github.com>
2019-10-23 10:31:45 +0700

    Add files via upload

--- baitap5.js
+++ baitap5.js
@@ -1,3 +1,5 @@
+// bai 1: in tung tu
+
 var loibaihat = ["kia","con","buom","vang","kia","con","buom","xanh"]
 
 for ( let i = 0; i < loibaihat.length ; i++){
@@ -5,3 +7,13 @@ for ( let i = 0; i < loibaihat.length ; i++){
         console.log(loibaihat[i], )
     }, 1000)
 }
+
+
+
+// bai 2: tinh tong a+b
+
+var aFunction = function(a,b){
+    tong = a+b;
+    return tong;
+}
+console.log(tong);
